23350495fb4b7134a37d1f11ccd615ae715b48b9
jasonlee529 <819151997@163.com>
2017-06-16 14:07:34 +0800

    update

--- spider-room-lianjia/src/main/java/cn/lee/housing/spider/lianjia/ErshoufangProcessor.java
+++ spider-room-lianjia/src/main/java/cn/lee/housing/spider/lianjia/ErshoufangProcessor.java
@@ -1,5 +1,7 @@
 package cn.lee.housing.spider.lianjia;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
@@ -12,9 +14,15 @@ public class ErshoufangProcessor implements PageProcessor {
 
     private Site site = Site.me().setRetryTimes(3).setSleepTime(1000);
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Override
     public void process(Page page) {
-        System.out.println(page.getHtml().xpath("//ul[@class='sellListContent']/text()"));
+        logger.error("======================");
+        logger.info(page.getHtml().$("div.leftContent").$("div.info div.title a").links().all().toString());
+        logger.info(page.getHtml().links().regex("https://bj.lianjia.com/ershoufang/[\\d$].html").all().toString());
+
+        page.addTargetRequests(page.getHtml().$("div.leftContent div.info div.title a").links().all());
     }
 
     @Override
@@ -26,7 +34,7 @@ public class ErshoufangProcessor implements PageProcessor {
 
         Spider.create(new ErshoufangProcessor())
                 //从"https://github.com/code4craft"开始抓
-                .addUrl("https://bj.lianjia.com/ershoufang")
+                .addUrl("https://bj.lianjia.com/ershoufang/")
                 //开启5个线程抓取
                 .thread(5)
                 //启动爬虫
