3a524759e390da39dc395bfa1c261fc47d2b1055
jasonlee529 <libo529@gmail.com>
2017-08-27 16:40:51 +0800

    测试

---
+++ spider-room-lianjia/src/test/java/TestRegx.java
@@ -0,0 +1,23 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+
+/**
+ * Created by jason on 17-8-27.
+ */
+
+public class TestRegx {
+    @Test
+    public void testReg() {
+        String str = "https://bj.lianjia.com/chengjiao/101101582869.html";
+        String pattern = "[1-9]\\d*";
+
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(str);
+
+        System.out.println(m.find());
+        System.out.println(m.group());
+    }
+
+}
