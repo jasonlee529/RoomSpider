8bd205f3e702158d0216e2bcb3ae10723937cd5e
jasonlee529 <libo529@gmail.com>
2017-09-04 22:55:17 +0800

    update scheduler

--- spider-room-lianjia/src/main/java/cn/lee/housing/spider/lianjia/web/ershoufang/ChengjiaoController.java
+++ spider-room-lianjia/src/main/java/cn/lee/housing/spider/lianjia/web/ershoufang/ChengjiaoController.java
@@ -3,16 +3,15 @@ package cn.lee.housing.spider.lianjia.web.ershoufang;
 import cn.lee.housing.spider.lianjia.service.ChengjiaoPipeline;
 import cn.lee.housing.spider.lianjia.service.proxy.ProxyService;
 import cn.lee.housing.spider.lianjia.spider.ChengjiaoProcessor;
-import us.codecraft.webmagic.Spider;
-import us.codecraft.webmagic.downloader.HttpClientDownloader;
-import us.codecraft.webmagic.pipeline.ConsolePipeline;
-import us.codecraft.webmagic.proxy.ProxyProvider;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import us.codecraft.webmagic.scheduler.FileCacheQueueScheduler;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.downloader.HttpClientDownloader;
+import us.codecraft.webmagic.pipeline.ConsolePipeline;
+import us.codecraft.webmagic.proxy.ProxyProvider;
+import us.codecraft.webmagic.scheduler.PriorityScheduler;
 
 
 /**
@@ -37,7 +36,8 @@ public class ChengjiaoController {
             HttpClientDownloader downloader = new HttpClientDownloader();
             downloader.setProxyProvider(mipuProxy);
             Spider spider = Spider.create(cjProcessor)
-                    .setScheduler(new FileCacheQueueScheduler("/home/web"))
+                   // .setScheduler(new FileCacheQueueScheduler("/home/web"))
+                    .setScheduler(new PriorityScheduler())
                     .addPipeline(new ConsolePipeline())
                     .addPipeline(pipeline)
                     .addUrl("https://bj.lianjia.com/chengjiao/changping");
