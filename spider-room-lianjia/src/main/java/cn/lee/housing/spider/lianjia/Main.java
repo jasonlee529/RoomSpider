ad76bec720134d93e77d10d57affb49df6d0736b
jasonlee529 <libo529@gmail.com>
2017-09-04 10:17:45 +0800

    update

--- spider-room-lianjia/src/main/java/cn/lee/housing/spider/lianjia/Main.java
+++ spider-room-lianjia/src/main/java/cn/lee/housing/spider/lianjia/Main.java
@@ -4,20 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * Created by jason on 17/7/14.
  */
-@Configuration
-@ComponentScan(value = {"cn.lee.housing"})
-@EnableAutoConfiguration
 @ImportResource(locations = "classpath*:applicationContext.xml")
-@EnableWebMvc
+@SpringBootApplication
 public class Main {
 
     private static Logger logger = LoggerFactory.getLogger(Main.class);
@@ -25,4 +19,5 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
+
 }
