2a4f1e5af7456a02c5c61a868b6b00e22a8ed774
jasonlee529 <libo529@gmail.com>
2017-10-13 19:42:25 +0800

    update jetty

---
+++ wx-pub/src/main/java/cn/lee/wx/Main.java
@@ -0,0 +1,28 @@
+package cn.lee.wx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by jason on 17-10-13.
+ */
+@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
+@ImportResource
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+
+    @RequestMapping("wx")
+    public String wx() {
+        return "";
+    }
+}
