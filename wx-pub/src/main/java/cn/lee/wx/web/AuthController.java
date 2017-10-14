f48620e6e607cd826ae4572e3950d9ef370fee05
JasonLee <libo529@gmail.com>
2017-10-14 22:25:14 +0800

    update

--- wx-pub/src/main/java/cn/lee/wx/web/AuthController.java
+++ wx-pub/src/main/java/cn/lee/wx/web/AuthController.java
@@ -1,5 +1,7 @@
 package cn.lee.wx.web;
 
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,29 +29,11 @@ public class AuthController {
                 signature, timestamp, nonce
         };
         Arrays.sort(list);
-//        DigestUtils.shaHex(list.toString());
-//        try:
-//        data = web.input()
-//        if len(data) == 0:
-//        return "hello, this is handle view"
-//        signature = data.signature
-//        timestamp = data.timestamp
-//        nonce = data.nonce
-//        echostr = data.echostr
-//        token = "xxxx" #请按照公众平台官网\基本配置中信息填写
-//
-//        list = [token, timestamp, nonce]
-//        list.sort()
-//        sha1 = hashlib.sha1()
-//        map(sha1.update, list)
-//        hashcode = sha1.hexdigest()
-//        print "handle/GET func: hashcode, signature: ", hashcode, signature
-//        if hashcode == signature:
-//        return echostr
-//            else:
-//        return ""
-//        except Exception, Argument:
-//        return Argument
-        return "fdsafs";
+        String hashCode = new String(DigestUtils.sha1(list.toString().getBytes()));
+        if (StringUtils.equals(signature, hashCode)) {
+            return echostr;
+        } else {
+            return "";
+        }
     }
 }
