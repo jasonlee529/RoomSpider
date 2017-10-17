69d34dc1c4818de0ef0879915fdd6cb26ccd6c2b
jasonlee529 <libo529@gmail.com>
2017-10-17 13:57:15 +0800

    update

--- spider-room-lianjia/src/main/java/cn/lee/housing/spider/lianjia/service/room/ChengjiaoService.java
+++ spider-room-lianjia/src/main/java/cn/lee/housing/spider/lianjia/service/room/ChengjiaoService.java
@@ -64,7 +64,7 @@ public class ChengjiaoService {
             HttpClientDownloader downloader = new HttpClientDownloader();
             downloader.setProxyProvider(proxyProvider);
             ChengjiaoProcessor processor = factory.getObject(area);
-            processor.setParams(new String[]{"ddo22" });
+           // processor.setParams(new String[]{"ddo22" });
             Spider spider = MySpider.create(processor)
                     .setScheduler(new PriorityScheduler())
                     .addPipeline(pipeline)
