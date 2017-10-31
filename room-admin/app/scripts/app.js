14b2c3def4451fbb55be1406a85bd85b7a1a0d4e
jasonlee529 <libo529@gmail.com>
2017-10-31 16:50:14 +0800

    update

---
+++ room-admin/app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name roomAdminApp
+ * @description
+ * # roomAdminApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('roomAdminApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ room-admin/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name roomAdminApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the roomAdminApp
+ */
+angular.module('roomAdminApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
