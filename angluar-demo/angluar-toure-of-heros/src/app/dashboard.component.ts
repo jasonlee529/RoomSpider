32b826e07786d9fc10ebcb6da6c2c49be0df7852
jasonlee529 <libo529@gmail.com>
2017-11-03 16:21:41 +0800

    update; g

--- angluar-demo/angluar-toure-of-heros/src/app/dashboard.component.ts
+++ angluar-demo/angluar-toure-of-heros/src/app/dashboard.component.ts
@@ -5,7 +5,7 @@ import {HeroService} from './hero.service';
 @Component({
   selector: 'my-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: [ './dashboard.component.css' ]
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
@@ -16,6 +16,10 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
     this.heroService.getHeroes()
-      .then(heroes => this.heroes = heroes.slice(1, 5));
+      .then(
+        heroes => {
+          console.log(heroes);
+          this.heroes = heroes.slice(1, 5)
+        });
   }
 }
