7873dedb30a6f4b6a92dd3800d9942a192165d31
jasonlee529 <libo529@gmail.com>
2018-01-02 12:00:59 +0800

    update

--- front-page/app/scripts/compiled-template.js
+++ front-page/app/scripts/compiled-template.js
@@ -1,2 +1,8 @@
-var template = require('art-template');
-var kpiTpl = template.compile("");
+var kpiTpl = template.compile(
+  ' <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">' +
+  '<span class="count_top"><i class="fa fa-user fa-{{clazz}}"></i> {{title}}</span>' +
+  '<div class="count">{{value}}</div>' +
+  '<span class="count_bottom"><i class="green">{{value2}} </i> {{subTitle}}</span>' +
+  '</div>');
+
+
--- front-page/app/scripts/main.js
+++ front-page/app/scripts/main.js
@@ -1,76 +1,12 @@
 $(document).ready(function () {
-  if ($('#mainb').length) {
-
-    var echartBar = echarts.init(document.getElementById('mainb'), theme);
-
-    echartBar.setOption({
-      title: {
-        text: 'Graph title',
-        subtext: 'Graph Sub-text'
-      },
-      tooltip: {
-        trigger: 'axis'
-      },
-      legend: {
-        data: ['sales', 'purchases']
-      },
-      toolbox: {
-        show: false
-      },
-      calculable: false,
-      xAxis: [{
-        type: 'category',
-        data: ['1?', '2?', '3?', '4?', '5?', '6?', '7?', '8?', '9?', '10?', '11?', '12?']
-      }],
-      yAxis: [{
-        type: 'value'
-      }],
-      series: [{
-        name: 'sales',
-        type: 'bar',
-        data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3],
-        markPoint: {
-          data: [{
-            type: 'max',
-            name: '???'
-          }, {
-            type: 'min',
-            name: '???'
-          }]
-        },
-        markLine: {
-          data: [{
-            type: 'average',
-            name: '???'
-          }]
-        }
-      }, {
-        name: 'purchases',
-        type: 'bar',
-        data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3],
-        markPoint: {
-          data: [{
-            name: 'sales',
-            value: 182.2,
-            xAxis: 7,
-            yAxis: 183,
-          }, {
-            name: 'purchases',
-            value: 2.3,
-            xAxis: 11,
-            yAxis: 3
-          }]
-        },
-        markLine: {
-          data: [{
-            type: 'average',
-            name: '???'
-          }]
-        }
-      }]
-    });
-
-  }
-
-
+  $.get("http://localhost:18080/data/sql/1", function (res) {
+    var d = res.result[0];
+    var data = [{clazz: 'user', title: '总成交量', value: d.total_deal},
+      {clazz: 'clock-o', title: '成交时间', value: d.last_deal_date, subTitle: '最早成交时间', value2: d.min_deal_date}
+    ]
+    $('#totalKpi').empty();
+    data.forEach(function (n) {
+      $('#totalKpi').append(kpiTpl(n));
+    })
+  });
 })
