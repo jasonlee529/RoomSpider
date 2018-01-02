ba5dc769b350e299444937c43f01d324af060431
jasonlee529 <libo529@gmail.com>
2018-01-02 13:52:32 +0800

    update

--- front-page/app/scripts/main.js
+++ front-page/app/scripts/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $.get("http://localhost:18080/data/sql/1", function (res) {
+  $.get('http://localhost:18080/data/sql/1', function (res) {
     var d = res.result[0];
     var data = [{clazz: 'user', title: '总成交量', value: d.total_deal},
       {clazz: 'clock-o', title: '成交时间', value: d.last_deal_date, subTitle: '最早成交时间', value2: d.min_deal_date}
