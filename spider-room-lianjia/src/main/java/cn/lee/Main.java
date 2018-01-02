a56937a6e97c95c0470e8c3655bd98238014deb8
jasonlee529 <libo529@gmail.com>
2018-01-02 12:01:17 +0800

    add cross origin

--- spider-room-lianjia/src/main/java/cn/lee/Main.java
+++ spider-room-lianjia/src/main/java/cn/lee/Main.java
@@ -6,8 +6,12 @@ import org.slf4j.LoggerFactory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by jason on 17/7/14.
@@ -23,4 +27,13 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }
