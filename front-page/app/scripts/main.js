ac3fce41a13a2e3c9d62354a8e5da4a85cca1f55
jasonlee529 <libo529@gmail.com>
2018-01-25 14:14:58 +0800

    update

--- front-page/app/scripts/main.js
+++ front-page/app/scripts/main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     var data = [{clazz: 'user', title: '总成交量', value: d.total_deal},
       {clazz: 'clock-o', title: '最新成交时间', value: d.last_deal_date, subTitle: '最早成交时间', value2: d.min_deal_date},
       {clazz: 'clock-o', title: '成交爬取爬取时间', value: d.last_craw_time.substr(0, 10), value2: d.last_craw_time.substr(10)},
-      {clazz: 'clock-o', title: '总二手房量', value: d.total_ershoufang},
+      {clazz: 'clock-o', title: '总挂牌量', value: d.total_ershoufang},
       {clazz: 'clock-o', title: '二手房爬取时间', value: d.max_list_craw.substr(0, 10), value2: d.max_list_craw.substr(11)},
       {clazz: 'clock-o', title: '报价爬取时间', value: d.max_baojia_craw.substr(0, 10), value2: d.max_baojia_craw.substr(10)}
     ]
