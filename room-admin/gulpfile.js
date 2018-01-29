2537f10969a6561c15860c8a990ec0a614c47e03
jasonlee529 <libo529@gmail.com>
2018-01-29 18:48:35 +0800

    update

--- room-admin/gulpfile.js
+++ room-admin/gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp'), //将上面down下来的插件都要引入，以便需要
     gutil = require('gulp-util'),
     wiredep = require('wiredep').stream;
-
+const del = require('del');
+const runSequence = require('run-sequence');
+const browserSync = require('browser-sync').create();
 
 gulp.task('wiredep', function () {  // 创建task任务:可以在cmd命令中执行任务
     gulp.src('src/main/index.html')
@@ -10,7 +12,24 @@ gulp.task('wiredep', function () {  // 创建task任务:可以在cmd命令中执
             goes: 'here'
         }))
         .pipe(gulp.dest('src/main'))
-})
+});
 
 
 
+gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
+
+gulp.task('serve', () => {
+    runSequence(['clean', 'wiredep'],  () => {
+        browserSync.init({
+            notify: false,
+            port: 9000,
+            server: {
+                baseDir: ['.tmp', 'app'],
+                routes: {
+                    '/bower_components': 'bower_components'
+                }
+            }
+        });
+
+    });
+});
