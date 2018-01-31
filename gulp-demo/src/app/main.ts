9b1c57567d5ea66b31d105e67885bdb831a5cf48
jasonlee529 <libo529@gmail.com>
2018-01-31 18:45:38 +0800

    update f

---
+++ gulp-demo/src/app/main.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {sayHello} from "./greet";
+
+
+@Component ({
+})
+
+export class main{
+    constructor() { }
+}
+console.log(sayHello("TypeScript"));
