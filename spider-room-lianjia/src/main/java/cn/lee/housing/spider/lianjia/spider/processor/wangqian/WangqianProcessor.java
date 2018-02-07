4ea1a7043f719fcc733a67b98e24fb4097c92a35
jasonlee529 <libo529@gmail.com>
2018-02-07 14:56:27 +0800

    update

--- spider-room-lianjia/src/main/java/cn/lee/housing/spider/lianjia/spider/processor/wangqian/WangqianProcessor.java
+++ spider-room-lianjia/src/main/java/cn/lee/housing/spider/lianjia/spider/processor/wangqian/WangqianProcessor.java
@@ -2,8 +2,8 @@ package cn.lee.housing.spider.lianjia.spider.processor.wangqian;
 
 import java.util.List;
 
-import cn.lee.housing.spider.lianjia.model.wangqian.DayInfo;
-import cn.lee.housing.spider.lianjia.model.wangqian.MonthInfo;
+import cn.lee.housing.spider.lianjia.model.wangqian.*;
+import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import us.codecraft.webmagic.Page;
@@ -22,26 +22,15 @@ public class WangqianProcessor implements PageProcessor {
 
     @Override
     public void process(Page page) {
+        page.putField("crawlMonth", parseMonth(page));
         page.putField("dayInfo", parseDayInfo(page));
         page.putField("monthAgent", parseMonthAgent(page));
         page.putField("monthArea", parseMonthArea(page));
         page.putField("monthCounty", parseMonthCounty(page));
-        page.putField("month", parseMonthInfo(page));
+        page.putField("monthInfo", parseMonthInfo(page));
     }
 
-    private Object parseMonthInfo(Page page) {
-        return null;
-    }
-
-    private Object parseMonthCounty(Page page) {
-        return null;
-    }
-
-    private Object parseMonthArea(Page page) {
-        return null;
-    }
-
-    private MonthInfo parseMonthAgent(Page page) {
+    private MonthInfo parseMonthInfo(Page page) {
         MonthInfo obj = new MonthInfo();
         Selectable table = page.getHtml().$("table.tjInfo[1]");
         obj.setMonth(table.$("thead tr").get().replace("存量房网上签约", ""));
@@ -50,6 +39,70 @@ public class WangqianProcessor implements PageProcessor {
         obj.setResAmt(table.$("tbody tr[2] td[1]").get());
         obj.setResAreas(table.$("tbody tr[3] td[1]").get());
         return obj;
+    }
+
+    private List<MonthCounty> parseMonthCounty(Page page) {
+        List<MonthCounty> result = Lists.newArrayList();
+        Selectable table = page.getHtml().$("table.tjInfo01[2]");
+        for (int i = 0; i < 6; i = i + 3) {
+            Selectable trTitle = table.$("tr[" + i + "]");
+            Selectable trAmt = table.$("tr[" + i + 1 + "]");
+            Selectable trArea = table.$("tr[" + i + 2 + "]");
+            int len = trTitle.$("td").nodes().size();
+            for (int j = 1; j < len; j++) {
+                MonthCounty mc = new MonthCounty();
+                mc.setMonth(parseMonth(page));
+                mc.setCounty(trTitle.$("td[" + j + "]").get());
+                mc.setDealAmt(trAmt.$("td[" + j + "]").get());
+                mc.setTotalArea(trArea.$("td[" + j + "]").get());
+                result.add(mc);
+            }
+        }
+        return result;
+    }
+
+    private List<MonthArea> parseMonthArea(Page page) {
+        List<MonthArea> result = Lists.newArrayList();
+        Selectable table = page.getHtml().$("table.tjInfo01[3]");
+        Selectable trArea = table.$("tr[0]");
+        Selectable trNewAmt = table.$("tr[1]");
+        Selectable trNeaArea = table.$("tr[2]");
+        Selectable trDealAmt = table.$("tr[3]");
+        Selectable trDealArea = table.$("tr[4]");
+        int len = table.$("tr[0] td").nodes().size();
+        for (int j = 1; j < len; j++) {
+            MonthArea mc = new MonthArea();
+            mc.setMonth(parseMonth(page));
+            mc.setAreaName(trArea.$("td[" + j + "]").get());
+            mc.setNewAmt(trNewAmt.$("td[" + j + "]").get());
+            mc.setNewAreas(trNeaArea.$("td[" + j + "]").get());
+            mc.setDealAmt(trDealAmt.$("td[" + j + "]").get());
+            mc.setDealAreas(trDealArea.$("td[" + j + "]").get());
+            result.add(mc);
+        }
+        return result;
+    }
+
+    private List<MonthAgent> parseMonthAgent(Page page) {
+        List<MonthAgent> result = Lists.newArrayList();
+        for (int i = 0; i < 2; i++) {
+            Selectable table = page.getHtml().$("table.tjInfo01").nodes().get(i);
+            for (Selectable tr : table.$("tbody tr").nodes()) {
+                MonthAgent agent = new MonthAgent();
+                agent.setMonth(parseMonth(page));
+                agent.setSeq(tr.$("td[0]").get());
+                agent.setAgent(tr.$("td[1]").get());
+                agent.setNewAmt(tr.$("td[2]").get());
+                agent.setDealAmt(tr.$("td[3]").get());
+                agent.setRefundAmt(tr.$("td[4]").get());
+                result.add(agent);
+            }
+        }
+        return result;
+    }
+
+    private String parseMonth(Page page) {
+        return page.getHtml().$("div.toolbar01").get();
     }
 
     private DayInfo parseDayInfo(Page page) {
