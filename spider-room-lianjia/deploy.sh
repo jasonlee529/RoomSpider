1e221057adb458e562928fb79aaa96be83563ded
libo <libo@infisan.cn>
2020-05-21 09:25:37 +0800

    修改脚本

--- spider-room-lianjia/deploy.sh
+++ spider-room-lianjia/deploy.sh
@@ -4,7 +4,7 @@ ps -ef|grep room.lianjia|awk '{print $2}'|xargs kill -9
 git pull
 
 mvn clean package
-JVM_OPT=-Xmx1G -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/home/web/logs/java.hprof
+JVM_OPT="-Xmx1G -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/home/web/logs/java.hprof"
 echo $JVM_OPT
 nohup  java -jar -Xmx1G $JVM_OPT target/room.lianjia-*.jar  >>t.log & tail -11f t.log
 
