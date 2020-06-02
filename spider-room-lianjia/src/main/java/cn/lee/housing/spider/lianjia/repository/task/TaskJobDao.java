d5e1471a14df0b4a6fe0caf5801ab757f0db5099
libo <libo@infisan.cn>
2020-06-02 19:38:35 +0800

    实现mybatis替换

--- spider-room-lianjia/src/main/java/cn/lee/housing/spider/lianjia/repository/task/TaskJobDao.java
+++
@@ -1,8 +0,0 @@
-package cn.lee.housing.spider.lianjia.repository.task;
-
-import cn.lee.housing.spider.lianjia.model.task.TaskJob;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface TaskJobDao extends PagingAndSortingRepository<TaskJob, Long>, JpaSpecificationExecutor<TaskJob> {
-}
